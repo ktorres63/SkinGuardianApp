e3fd465a6d6258455cc05a064a03b2ceac18dab9
ktorres63 <ktorresaro@unsa.edu.pe>
2023-11-22 18:56:14 -0500

    add some colors

---
+++ app/src/main/java/com/idnp/skinguardianapp/RoutineListActivity.kt
@@ -0,0 +1,11 @@
+package com.idnp.skinguardianapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RoutineListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_routine_list)
+    }
+}
