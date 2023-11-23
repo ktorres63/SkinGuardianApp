5179af5aafee7476c7b52f5aefd72d5b31f60830
ktorres63 <ktorresaro@unsa.edu.pe>
2023-11-23 10:43:50 -0500

    add progress in Login Activity

--- app/src/main/java/com/idnp/skinguardianapp/RecomendationListActivity.kt
+++ app/src/main/java/com/idnp/skinguardianapp/RecomendationListActivity.kt
@@ -2,6 +2,7 @@ package com.idnp.skinguardianapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class RecomendationListActivity : AppCompatActivity() {
     private lateinit var btnRemedy: AppCompatButton
@@ -10,7 +11,7 @@ class RecomendationListActivity : AppCompatActivity() {
     private lateinit var btnReturn: AppCompatButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_recommendations)
+        setContentView(R.layout.activity_recomendation_list)
 
         initComponent()
     }
