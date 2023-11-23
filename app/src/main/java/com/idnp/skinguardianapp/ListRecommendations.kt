f86d52f867f4e16f8b4f666c8aff1da58676f987
Andy Briyit <ahuamanita@unsa.edu.pe>
2023-11-23 00:42:08 -0500

    se creo el activity para la lista de recomendaciones

---
+++ app/src/main/java/com/idnp/skinguardianapp/ListRecommendations.kt
@@ -0,0 +1,11 @@
+package com.idnp.skinguardianapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListRecommendations : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_recommendations)
+    }
+}
