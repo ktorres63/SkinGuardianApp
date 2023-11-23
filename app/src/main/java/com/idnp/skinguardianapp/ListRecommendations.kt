637af670d36ac9a769b24c6449bba64475a107f6
Julio <jpaucarc@unsa.edu.pe>
2023-11-23 10:29:38 -0500

    Corrección de errores y agregación de botones para la futura implementación

--- app/src/main/java/com/idnp/skinguardianapp/ListRecommendations.kt
+++ app/src/main/java/com/idnp/skinguardianapp/ListRecommendations.kt
@@ -2,10 +2,24 @@ package com.idnp.skinguardianapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class ListRecommendations : AppCompatActivity() {
+    private lateinit var btnRemedy: AppCompatButton
+    private lateinit var btnDiet: AppCompatButton
+    private lateinit var btnExercise: AppCompatButton
+    private lateinit var btnReturn: AppCompatButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_recommendations)
+
+        initComponent()
+    }
+
+    private fun initComponent() {
+        btnRemedy = findViewById(R.id.btnRemedy)
+        btnDiet = findViewById(R.id.btnDiet)
+        btnExercise = findViewById(R.id.btnExercise)
+        btnReturn = findViewById(R.id.btnReturn)
     }
 }
