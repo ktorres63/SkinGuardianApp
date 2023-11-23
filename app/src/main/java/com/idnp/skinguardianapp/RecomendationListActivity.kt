31a286038ac4edb54d93a557f33c74b483c606f0
ktorres63 <ktorresaro@unsa.edu.pe>
2023-11-23 10:36:22 -0500

    correction in RecomendationListActivity

--- app/src/main/java/com/idnp/skinguardianapp/RecomendationListActivity.kt
+++ app/src/main/java/com/idnp/skinguardianapp/RecomendationListActivity.kt
@@ -4,8 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class RecomendationListActivity : AppCompatActivity() {
+    private lateinit var btnRemedy: AppCompatButton
+    private lateinit var btnDiet: AppCompatButton
+    private lateinit var btnExercise: AppCompatButton
+    private lateinit var btnReturn: AppCompatButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recomendation_list)
+        setContentView(R.layout.activity_list_recommendations)
+
+        initComponent()
+    }
+
+    private fun initComponent() {
+        btnRemedy = findViewById(R.id.btnRemedy)
+        btnDiet = findViewById(R.id.btnDiet)
+        btnExercise = findViewById(R.id.btnExercise)
+        btnReturn = findViewById(R.id.btnReturn)
     }
 }
