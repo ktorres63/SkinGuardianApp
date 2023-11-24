f79322a7022c454aa7098b7765df2f6131513c09
ktorres63 <ktorresaro@unsa.edu.pe>
2023-11-23 21:10:48 -0500

    fixing errors in activityLogin layout

--- app/src/main/java/com/idnp/skinguardianapp/LoginActivity.kt
+++ app/src/main/java/com/idnp/skinguardianapp/LoginActivity.kt
@@ -1,11 +1,43 @@
 package com.idnp.skinguardianapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
+
+    // Declarar los EditText
+    private lateinit var usuarioEditText: EditText
+    private lateinit var contraseniaEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val ingresarButton: Button = findViewById(R.id.buttonIngresar)
+        ingresarButton.setOnClickListener(View.OnClickListener {
+
+
+            usuarioEditText = findViewById(R.id.idUsuario)
+            contraseniaEditText = findViewById(R.id.idContrasenia)
+
+            val usuario: String = usuarioEditText.text.toString()
+            val contrasenia: String = contraseniaEditText.text.toString()
+
+            if (usuario == "73321272" && contrasenia == "1234") {
+                val intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "Usuario o contraseña incorrectos",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
     }
 }
