f8f2f1fd1ad66b79a20fe74a4191f13bdaf0702a
ktorres63 <ktorresaro@unsa.edu.pe>
2023-12-06 21:41:25 -0500

    add binding to fragments

--- app/src/main/java/com/idnp/skinguardianapp/ui/routines/RoutinesFragment.kt
+++ app/src/main/java/com/idnp/skinguardianapp/ui/routines/RoutinesFragment.kt
@@ -5,16 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentResultListener
 import com.idnp.skinguardianapp.R
+import com.idnp.skinguardianapp.databinding.FragmentRoutinesBinding
 
 
 class RoutinesFragment : Fragment() {
-
+    private var _binding: FragmentRoutinesBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_routines, container, false)
+       _binding = FragmentRoutinesBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 }
